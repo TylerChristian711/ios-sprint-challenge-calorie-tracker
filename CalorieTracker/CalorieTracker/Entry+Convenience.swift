b0c46867c2f9da8228bc73415f11191572f0d781
Tyler Christian <tylerc71197@gmail.com>
2020-01-31 14:02:33 -0500

    started the data model

---
+++ CalorieTracker/CalorieTracker/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  CalorieTracker
+//
+//  Created by Lambda_School_Loaner_218 on 1/31/20.
+//  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import Foundation
---
+++ CalorieTracker/CalorieTracker/EntryRepresentation.swift
@@ -0,0 +1,14 @@
+//
+//  EntryRepresentation.swift
+//  CalorieTracker
+//
+//  Created by Lambda_School_Loaner_218 on 1/31/20.
+//  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import Foundation
+
+struct EntryRepresentation: Codable {
+    var calories: Float
+    var timestamp: Date
+}
